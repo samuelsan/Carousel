ed34ee41a80ac57279f45ddc91e9212c825922c2
Samuel San <samuelsaninbox@gmail.com>
2015-08-06 20:30:02 +0000

    added random linear movements

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,26 @@
+var FireFly = function (game, x, y) {
+
+    Phaser.Sprite.call(this, game, x, y, 'firefly');
+
+    // this.rotateSpeed = rotateSpeed;
+
+    this.game.time.events.loop(500, function() {
+    this.game.add.tween(this).to({x: this.game.world.randomX, y: this.game.world.randomY}, 450, Phaser.Easing.Quadratic.InOut, true);
+    }, this)
+};
+
+
+
+FireFly.prototype = Object.create(Phaser.Sprite.prototype);
+FireFly.prototype.constructor = FireFly;
+
+FireFly.prototype.update = function() {
+
+};
+
 (function () {
   var bugnet, background, firefly;
+
   var handlers = {
     preload: function () {
       game.load.image('background', 'assets/temp_minigamebackground.png');
@@ -12,30 +33,45 @@
       background = game.add.image(0,0, 'background');
       bugjar = game.add.image(0,0, 'bugjar')
 
-      var group = game.add.group();
+      // var group = game.add.group(); 
 
-      for (var i = 0; i < 6; i++)
-      {
-        //  They are evenly spaced out on the X coordinate, with a random Y coordinate
-        firefly = group.create(120 * i, game.rnd.integerInRange(100, 400), 'firefly');
-      }
-      game.physics.enable(group, Phaser.Physics.ARCADE);
-      firefly.body.collideWorldBounds = true;
-      firefly.body.bounce.setTo(1, 1);
+      // for (var i = 0; i < 6; i++)
+      // {
+      //   //  They are evenly spaced out on the X coordinate, with a random Y coordinate
+      //   firefly = group.create(120 * i, game.rnd.integerInRange(100, 400), 'firefly');
+      // }
+
+    var firefly = new FireFly(game, 200, 300);
+
+    var firefly2 = new FireFly(game, 300, 400);
+
+    var firefly3 = new FireFly(game, 400, 400);
+
+    var firefly4 = new FireFly(game, 500, 400);
+
+    var firefly5 = new FireFly(game, 600, 400);
+
+    var firefly6 = new FireFly(game, 300, 400);
+
+    game.add.existing(firefly);
+    game.add.existing(firefly2);
+    game.add.existing(firefly3);
+    game.add.existing(firefly4);
+    game.add.existing(firefly5);
+    game.add.existing(firefly6);
 
       bugnet = game.add.sprite(400, 300, 'bugnet');
-      bugnet.name = 'bugnet';
       bugnet.anchor.setTo(0.5, 0.5);
       game.physics.enable(bugnet, Phaser.Physics.ARCADE);
-      bugnet.body.collideWorldBounds = true;
-      bugnet.body.checkCollision.up = false;
-      bugnet.body.checkCollision.down = false;
       bugnet.body.allowRotation = false;
     },
+    
     update: function () {
+      // console.log('updating')
       bugnet.rotation = game.physics.arcade.moveToPointer(bugnet, 0, game.input.activePointer, 100);
     }
   };
+
   var game = new Phaser.Game(1000, 800, Phaser.AUTO, 'phaser-example', {
     preload: handlers.preload,
     create: handlers.create,
