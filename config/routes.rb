5e1019fc32ceaa28e541b868ff0b554654924942
Noffica <nmithani1@gmail.com>
2015-08-13 16:59:30 -0700

    Display 'Play Demo' link IF current_user. Sign out link IF current_user.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # get 'public/index'
-  # get 'carousel/index'
+  root  'public#index'
+  
+  get   'public/index'
+  get   'carousel/index'
 
   resources :users
   resource  :session,   only: [:create, :destroy]
@@ -9,7 +11,6 @@ Rails.application.routes.draw do
   match 'auth/failure',             to: redirect('/'),                      via: [:get, :post]
   match 'signout',                  to: 'sessions#destroy', as: 'signout',  via: [:get, :post]
 
-  root 'public#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
