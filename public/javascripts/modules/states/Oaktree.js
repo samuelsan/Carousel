98f7839903b3565b69493c1e65442bfa96c2b256
Samuel San <samuelsaninbox@gmail.com>
2015-08-13 19:06:48 +0000

    acorns now dissapear when hitting squirrelhole

--- public/javascripts/modules/states/Oaktree.js
+++ public/javascripts/modules/states/Oaktree.js
@@ -7,11 +7,13 @@ var OaktreeState = function (game)
   this.game = game;
   this.launchX = 320;
   this.launchY = 325;
+  this.acorn;
+  this.squirrelhole;
 };
 
-var shapes = [[1,796.5,929,558.5,1067,564,1067,800],[367,583.5,469,575.5,599,600.5,329,599.5,348.5,586],[824,582.5,735,606.5,780,586.5],[599,600.5,469,575.5,545,579.5,573,585.5],[61,621.5,1,796.5,10,632.5,17,624.5],[1067,564,1020,561.5,1052,559.5],[929,558.5,824,582.5,882,562.5],[329,599.5,1,796.5,259,603.5,285,597.5],[209,607.5,1,796.5,179,610.5,187,607.5],[677,611.5,1,796.5,631,606.5,667,607.5],[259,603.5,1,796.5,209,607.5,221,603.5],[469,575.5,367,583.5,391,575.5],[179,610.5,1,796.5,87,615.5,99,609.5],[10,632.5,1,796.5,0.5,636],[87,615.5,1,796.5,61,621.5],[599,600.5,631,606.5,1,796.5,329,599.5],[929,558.5,735,606.5,824,582.5],[703,610.5,1,796.5,677,611.5],[735,606.5,1,796.5,703,610.5]];
+var ground = [[1,796.5,929,558.5,1067,564,1067,800],[367,583.5,469,575.5,599,600.5,329,599.5,348.5,586],[824,582.5,735,606.5,780,586.5],[599,600.5,469,575.5,545,579.5,573,585.5],[61,621.5,1,796.5,10,632.5,17,624.5],[1067,564,1020,561.5,1052,559.5],[929,558.5,824,582.5,882,562.5],[329,599.5,1,796.5,259,603.5,285,597.5],[209,607.5,1,796.5,179,610.5,187,607.5],[677,611.5,1,796.5,631,606.5,667,607.5],[259,603.5,1,796.5,209,607.5,221,603.5],[469,575.5,367,583.5,391,575.5],[179,610.5,1,796.5,87,615.5,99,609.5],[10,632.5,1,796.5,0.5,636],[87,615.5,1,796.5,61,621.5],[599,600.5,631,606.5,1,796.5,329,599.5],[929,558.5,735,606.5,824,582.5],[703,610.5,1,796.5,677,611.5],[735,606.5,1,796.5,703,610.5]];
 
-
+var treetrunk = [[816.5, 330  ,  828.5, 294  ,  838.5, 311  ,  842.5, 325  ,  840.5, 339  ,  833, 352.5  ], [   737, 237.5  ,  693, 276.5  ,  710, 243.5  ,  717, 232.5  ], [   788.5, 337  ,  785.5, 323  ,  828.5, 294  ,  816.5, 330  ,  803.5, 346  ,  797, 349.5  ]];
 
 
 OaktreeState.prototype = {
@@ -35,7 +37,7 @@ OaktreeState.prototype = {
     // this.groundBody = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0, 0);
     // this.groundBody.setChain(groundVertices);
   
-    var PTM = 50;
+    // var PTM = 50;
   
 
     this.background = this.game.add.image(0,0, 'background');
@@ -76,6 +78,21 @@ OaktreeState.prototype = {
 
     // this.groundCollider.setRectangle(30, 30, 0, 0, 0);
 
+    this.treetrunkCollider = new Phaser.Physics.Box2D.Body(this.game, null, 0, 0);
+    this.treetrunkCollider.static = true;
+    this.treetrunkCollider.setChain(treetrunk);
+
+    // this.acorn.body.setBodyContactCallback(this.acorn, squirrelholeCallback, this);
+    
+    // function squirrelholeCallback(body1, body2, fixture1, fixture2, begin) {
+    //   if (!begin)
+    //   {
+    //     return;
+    //   }
+
+    //   this.acorn.destroy();
+    // }
+
     function flatten(arr)
     {
       return arr.reduce(function(a, i) { return a.concat(i); }, []); // .inject([]) { |a,i| a << i }
@@ -115,11 +132,10 @@ OaktreeState.prototype = {
       }
     }
 
-
     var points =  
       fromPairs(
         toPairs(
-          flatten(shapes)
+          flatten(ground)
         )
         .filter(band('y', 0, 700))
         .sort(byCoordinate('x'))
@@ -131,13 +147,14 @@ OaktreeState.prototype = {
   {
     // game.debug.box2dWorld();
   },
-  collisionHandler: function(acorn, ground, acornBody, groundBody, touching, contact)
-  {
-    // console.log("Collided", this, acorn, ground);
-    // acorn.kill();
-  },
+
   fireAcorn: function () {
-    var acorn = window.acorn = new Projectile(this.game, this.launchX, this.launchY);
-    acorn.body.setBodyContactCallback(this.groundCollider, this.collisionHandler, this);
-  }
+    this.acorn = window.acorn = new Projectile(this.game, this.launchX, this.launchY);
+    this.acorn.body.setBodyContactCallback(this.squirrelhole, this.squirrelholeCallback, this); 
+  },
+  squirrelholeCallback: function(body1, body2, fixture1, fixture2, begin) 
+  {
+    this.acorn.destroy();
+  }   
+
 };
