b59a8d376b2145b8f7595f068211fd581b9aea02
Noffica <nmithani1@gmail.com>
2015-08-15 18:03:55 -0700

    A basic Contact Us form template is in place. The form does not send an e-mail yet, though it should. Further work is needed.

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,18 @@ Rails.application.routes.draw do
   get   'public/synopsis' #Synopsis
   get   'public/team'     #Team page
   get   'public/about'    #About (the game)
-  get   'public/contact'  #Contact us form page
+  # get   'public/contact'  #Contact us form page
 
+
+  resources :contacts, only: [:new, :create]
   resources :users
+  
   resource  :session,   only: [:create, :destroy]
 
-  match 'auth/:provider/callback',  to: 'sessions#create',                  via: [:get, :post]
-  match 'auth/failure',             to:  redirect('/'),                      via: [:get, :post]
-  match 'signout',                  to: 'sessions#destroy', as: 'signout',  via: [:get, :post]
+  match '/contacts',                to: 'contacts#new',                    via: 'get'
+  match 'auth/:provider/callback',  to: 'sessions#create',                 via: [:get, :post]
+  match 'auth/failure',             to:  redirect('/'),                    via: [:get, :post]
+  match 'signout',                  to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
