f530c62c5364febbcaa741d8dfa97092ac412e50
kodester <jeremyzcheng@gmail.com>
2015-08-19 20:52:10 +0000

    endgame start

--- public/javascripts/modules/states/EndDemo.js
+++ public/javascripts/modules/states/EndDemo.js
@@ -1,18 +1,22 @@
-/* globals Phaser */
+/* globals Phaser, BootState */
 'use strict';
 
   var EndDemoState = function (game) {
     this.game = game;
-    var content = [
+
+    this.content = [
     "Thank you for playing Carousel!",
     "Hope you enjoy!",
     ];
-    var text;
-    var index = 0;
-    var line = '';
+    this.text;
+    this.index = 0;
+    this.line = '';
   };
 
+
+
   EndDemoState.prototype = {
+    constructor:BootState,
     preload: function() {
     this.game.load.image('background', '/javascripts/modules/units/backgrounds/stream.jpg');
 
@@ -20,38 +24,39 @@
 
     create: function() {
       this.background = this.game.add.image(0,0, 'background');
+      this.background.height = this.game.height;
+      this.background.width = this.game.width;
 
-      // text = game.add.text(32, 500, '', { font: "30pt Courier", fill: "black", stroke: "white", strokeThickness: 2 });
-      // nextLine();
+      this.text = this.game.add.text(32, 500, '', { font: "30pt Courier", fill: "black", stroke: "white", strokeThickness: 2 });
+      this.nextLine();
 
     },
     
-    // update: function() 
-    // {
-      //     if (line.length < content[index].length);
-      // {
-      //     line = content[index].substr(0, line.length + 1);
-      //     // text.text = line;
-      //     text.setText(line);
-      // }
-      // else
-      // {
-      //     //  Wait 2 seconds then start a new line
-      //     game.time.events.add(Phaser.Timer.SECOND * 2, nextLine, this);
-      // }
+    updateLine: function() 
+    {
+      if (this.line.length < this.content[this.index].length);
+      {
+          this.line = this.content[this.index].substr(0, this.line.length + 1);
+          // text.text = line;
+          this.text.setText(this.line);}
 
-    // };
+      else
+      {
+          //  Wait 2 seconds then start a new line
+          this.game.time.events.add(Phaser.Timer.SECOND * 2, this.nextLine, this);
+      }
 
-    // function nextLine() {
+    },
 
-    // index++;
+    nextLine: function()
+    {
 
-    // if (index < content.length);
-    // {
-    //     line = '';
-    //     game.time.events.repeat(80, content[index].length + 1, updateLine, this);
-    // };
+      this.index++;
 
-
-  //   },
+      if (this.index < this.content.length);
+      {
+          this.line = '';
+          this.game.time.events.repeat(80, this.content[this.index].length + 1, this.updateLine, this);
+      }
+    }
   };
