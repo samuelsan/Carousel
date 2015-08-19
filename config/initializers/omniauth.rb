112cafc50011e627cbadb430f4595b13b59ca417
Noffica <nmithani1@gmail.com>
2015-08-19 13:41:06 -0700

    Failed attempt at using Figaro gem.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,10 @@
 OmniAuth.config.logger = Rails.logger
 
+OmniAuth.app_id     = ENV["facebook_app_id"]
+OmniAuth.secret_key = ENV["facebook_secret_key"]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '125525414456584', '83eb8ccea60481bb49b9e8629f0ad12e', :info_fields => 'name,email,age_range'
+  provider :facebook, OmniAuth.app_id, OmniAuth.secret_key, :info_fields => 'name,email,age_range'
   # replace the above line with
   # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
 end
