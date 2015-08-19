6bf95eef32e02f47a1139992798bea051cf1e5ce
Noffica <nmithani1@gmail.com>
2015-08-19 16:55:36 -0700

    .env file added to .gitignore.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,4 @@
 Rails.application.configure do
-  puts "Development rb"
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,3 @@
-puts "Asserts rb"
-
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
