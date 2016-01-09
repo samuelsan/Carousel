2fee7ae76197fcbf31d0bce1e7651415f5c458f0
samuelsaninbox@gmail.com <samuelsaninbox@gmail.com>
2016-01-09 01:48:21 +0000

    added fb app id

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET_KEY"], :info_fields => 'name,email,age_range'
+  provider :facebook, ENV[1779030232324070], ENV[74199ee64583851b10d22e867a4e4d56], :info_fields => 'name,email,age_range'
 end
 
 OmniAuth.config.on_failure = Proc.new do |env|
